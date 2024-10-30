ac2ce1c3e71ea79cb3e4a8c270d83f5a3f915477
Samuelcitalan <131422504+Samuelcitalan@users.noreply.github.com>
2024-10-30 17:54:14 -0600

    pantalla de inicio

--- src/Logica/Material.java
+++ src/Logica/Material.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Logica;
 
 /**
@@ -47,4 +44,3 @@ public class Material {
         System.out.println("Cantidad: " + cantidad); // Imprime la cantidad del material.
     }
 }
-

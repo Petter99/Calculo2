ed6d089561b6ca0df963f3a3e0d3b49fbd1d7344
Petter99 <131422787+Petter99@users.noreply.github.com>
2024-11-20 13:38:00 -0600

    Update Main.java

--- src/Logica/Main.java
+++ src/Logica/Main.java
@@ -13,21 +13,6 @@ import GUI.Inicio;
 //Despues se tendra que eliminar
 public class Main {
     public static void main(String[] args) {
-        // Crea una instancia de ProyectoCuantificacion para gestionar el proyecto.
-        ProyectoCuantificacion proyecto = new ProyectoCuantificacion();
-        
-        // Crea una instancia de Hierro con nombre, cantidad de varillas, factor y varillas por metro.
-        Hierro hierro = new Hierro("Cimiento", 4, 5.4, 0.55);
-
-        // Crea una instancia de Concreto con metros cúbicos, arena, piedrín y cemento.
-        Concreto concreto = new Concreto(6.6, 4.95, 3.3, 60);
-
-        // Agrega los materiales al proyecto.
-        proyecto.agregarMaterial(hierro);
-        proyecto.agregarMaterial(concreto);
-        
-        // Calcula y muestra la cantidad total de materiales.
-        proyecto.calcularTotalMateriales();
         
         //Se crea un objeto ventana y se hace visible (menu inicial)
         Inicio ventana = new Inicio();
